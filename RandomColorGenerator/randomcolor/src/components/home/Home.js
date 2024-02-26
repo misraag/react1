9d2f85d996e5dce698d0019cee3a192da3cd2ad8
misraag <jaiofficial50@gmail.com>
2024-02-26 21:34:10 +0530

    added buttons and css

--- RandomColorGenerator/randomcolor/src/components/home/Home.js
+++ RandomColorGenerator/randomcolor/src/components/home/Home.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import styles from './Home.module.css'
 
 function Home () {
 
+    function handleRandomHexGenerator () {
+
+    }
+
+    function handleRandomRGBGenerator () {
+
+    }
+
+    function handleChangeColor () {
+
+    }
+
+
     return (
         <div>
-            Inside Home
+            <nav className={styles.navbar}>
+                <button onClick={handleRandomHexGenerator} className={styles.button}>Generate HEX</button>
+                <button onClick={handleRandomRGBGenerator} className={styles.button}>Generate RGB</button>
+                <button onClick={handleChangeColor} className={styles.button}>Change Color</button>
+            </nav>
+            <div className={styles.division}></div>
         </div>
     )
 
