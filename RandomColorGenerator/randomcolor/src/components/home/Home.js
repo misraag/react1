6fb79c7a2acd9af030d513b92b116841f082921c
misraag <jaiofficial50@gmail.com>
2024-02-28 21:46:16 +0530

    random hex generator created

--- RandomColorGenerator/randomcolor/src/components/home/Home.js
+++ RandomColorGenerator/randomcolor/src/components/home/Home.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Home.module.css'
 
 function Home () {
 
+    const [colorType, setColorType] = useState('HEX');
+    const [color, setColor] = useState('#000000');
+
+    function generateRandomNumber (length) {
+        return Math.floor(Math.random()*length);
+    }
+
+
     function handleRandomHexGenerator () {
+        console.log('Handling random hex generator');
+        const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+
+        let hexColor = '#';
+
+        for (let i = 0; i < 6; i++) {
+            hexColor += hex[generateRandomNumber(hex.length)];
+        }
+        console.log(color);
+        setColor(hexColor);
 
     }
 
@@ -11,19 +29,15 @@ function Home () {
 
     }
 
-    function handleChangeColor () {
-
-    }
-
 
     return (
         <div>
             <nav className={styles.navbar}>
-                <button onClick={handleRandomHexGenerator} className={styles.button}>Generate HEX</button>
-                <button onClick={handleRandomRGBGenerator} className={styles.button}>Generate RGB</button>
-                <button onClick={handleChangeColor} className={styles.button}>Change Color</button>
+                <button onClick={() => setColorType('HEX')} className={styles.button}>Create HEX</button>
+                <button onClick={() => setColorType('RGB')} className={styles.button}>Create RGB</button>
+                <button onClick={ colorType === 'HEX' ? handleRandomHexGenerator : handleRandomRGBGenerator} className={styles.button}>Generate Random Color</button>
             </nav>
-            <div className={styles.division}></div>
+            <div style={{backgroundColor: color}} className={styles.division}></div>
         </div>
     )
 
